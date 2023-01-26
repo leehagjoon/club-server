58c158e5e0a9b87297073ac51d52292a5fdd470f
leehagjoon(이학준) <winner__loser@naver.com>
2023-01-26 12:01:36 +0900

    init clone

---
+++ src/main/java/com/here/ClubServerApplication.java
@@ -0,0 +1,13 @@
+package com.here;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/here/ClubServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.here;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
