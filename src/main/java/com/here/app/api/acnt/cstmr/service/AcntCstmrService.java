8b242f7fd999f89676eff639b048a54a3023f67b
leehagjoon(이학준) <winner__loser@naver.com>
2023-01-31 14:42:01 +0900

    회원가입 수정

--- src/main/java/com/here/app/api/acnt/cstmr/service/AcntCstmrService.java
+++ src/main/java/com/here/app/api/acnt/cstmr/service/AcntCstmrService.java
@@ -91,6 +91,7 @@ public class AcntCstmrService {
 
     public ClubCstmrDtl savePtyCstmrDtl(AcntCstmrRqModel rq , ClubCstmrBas basEntity) throws Exception{
         ClubCstmrDtl dtlEntity = new ClubCstmrDtl();
+        dtlEntity.setPosition(rq.getPosition());
         dtlEntity.setCstmrSno(basEntity.getCstmrSno());
         dtlEntity.setGenderCd(rq.getGenderCd());
         dtlEntity.setMemberName(rq.getMemberName());
